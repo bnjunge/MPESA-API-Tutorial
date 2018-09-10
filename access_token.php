5e6bdbca81901b95aa8634888b355e95480b04c8
Ben Njunge <34906115+bnjunge@users.noreply.github.com>
2018-09-11 00:19:18 +0300

    Create access_token.php
    Using this code, you can obtain an access token for use in our next steps in the use of the API.

---
+++ access_token.php
@@ -0,0 +1,26 @@
+<?php 
+
+	$consumerKey = ''; //Replace with your app Consumer Key
+	$consumerSecret = ''; // Replace with your app Secret
+
+	$headers = ['Content-Type:application/json; charset=utf8'];
+
+	$url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
+	curl_setopt($curl, CURLOPT_HEADER, FALSE);
+
+	curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+
+	$result = curl_exec($curl);
+	$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	$result = json_decode($result);
+
+	$access_token = $result->access_token;
+
+	echo $access_token;
+	
+	curl_close($curl);
