d72c92ce07ed85540bcffe0091007e21973bc637
Ben Njunge <34906115+bnjunge@users.noreply.github.com>
2018-09-11 17:11:36 +0300

    Update register_url.php

--- register_url.php
+++ register_url.php
@@ -1,12 +1,14 @@
 <?php
-  $url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
-  $shortCode = ''; // provide the short code obtained from your test credentials
-  
-  /* This two files are provided in the project. */
-  $confirmationUrl = ''; // path to your confirmation url. can be IP address that is publicly accessible or a url
-  $validationUrl = ''; // path to your validation url. can be IP address that is publicly accessible or a url
-  
-  
+	$url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
+
+	$access_token = ''; // check the mpesa_accesstoken.php file for this. No need to writing a new file here, just combine the code as in the tutorial.
+	$shortCode = ''; // provide the short code obtained from your test credentials
+
+	/* This two files are provided in the project. */
+	$confirmationUrl = ''; // path to your confirmation url. can be IP address that is publicly accessible or a url
+	$validationUrl = ''; // path to your validation url. can be IP address that is publicly accessible or a url
+
+
 
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $url);
