1fe3ecb32a5f23b0269e515e69e6c87ccbae7c4e
Ben Njunge <34906115+bnjunge@users.noreply.github.com>
2018-09-11 17:18:47 +0300

    Create simulate.php
    This piece of code is used to simulate a transaction on the M-PESA API and the responses will be sent to validation/confirmation URLs

---
+++ simulate.php
@@ -0,0 +1,34 @@
+<?php
+    $url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate';
+    
+    $access_token = ''; // check file mpesa_accesstoken.php.
+    
+    $ShortCode  = ''; // Shortcode. Same as the one on register_url.php
+    $amount     = ''; // amount the client/we are paying to the paybill
+    $msisdn     = ''; // phone number paying 
+    $billRef    = ''; // This is anything that helps identify the specific transaction. Can be a clients ID, Account Number, Invoice amount, cart no.. etc
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$access_token));
+
+
+    $curl_post_data = array(
+           'ShortCode' => $ShortCode,
+           'CommandID' => 'CustomerPayBillOnline',
+           'Amount' => $amount,
+           'Msisdn' => $msisdn,
+           'BillRefNumber' => $billRef
+    );
+
+    $data_string = json_encode($curl_post_data);
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+    $curl_response = curl_exec($curl);
+    print_r($curl_response);
+
+    echo $curl_response;
+?>
