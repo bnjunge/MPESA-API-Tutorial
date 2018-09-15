515e1c25e3b483982a34746c508f5b7d91bc6ee2
Ben Njunge <34906115+bnjunge@users.noreply.github.com>
2018-09-15 07:59:24 +0300

    Update confirmation_url.php

--- confirmation_url.php
+++ confirmation_url.php
@@ -1,4 +1,5 @@
 <?php
+    require 'config.php';
     header("Content-Type: application/json");
 
     $response = '{
@@ -15,6 +16,23 @@
 
     $jsonMpesaResponse = json_decode($mpesaResponse, true); // We will then use this to save to database
 
+    $transaction = array(
+            ':TransactionType'      => $jsonMpesaResponse['TransactionType'],
+            ':TransID'              => $jsonMpesaResponse['TransID'],
+            ':TransTime'            => $jsonMpesaResponse['TransTime'],
+            ':TransAmount'          => $jsonMpesaResponse['TransAmount'],
+            ':BusinessShortCode'    => $jsonMpesaResponse['BusinessShortCode'],
+            ':BillRefNumber'        => $jsonMpesaResponse['BillRefNumber'],
+            ':InvoiceNumber'        => $jsonMpesaResponse['InvoiceNumber'],
+            ':OrgAccountBalance'    => $jsonMpesaResponse['OrgAccountBalance'],
+            ':ThirdPartyTransID'    => $jsonMpesaResponse['ThirdPartyTransID'],
+            ':MSISDN'               => $jsonMpesaResponse['MSISDN'],
+            ':FirstName'            => $jsonMpesaResponse['FirstName'],
+            ':MiddleName'           => $jsonMpesaResponse['MiddleName'],
+            ':LastName'             => $jsonMpesaResponse['LastName']
+    );
+
+
     // write to file
     $log = fopen($logFile, "a");
 
@@ -22,4 +40,6 @@
     fclose($log);
 
     echo $response;
+
+    insert_response($transaction);
 ?>
