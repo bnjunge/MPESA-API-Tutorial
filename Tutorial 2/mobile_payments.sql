2fd8963cf903caffe79d1a6d0400d6d176d9d2ce
Ben Njunge <34906115+bnjunge@users.noreply.github.com>
2018-09-15 10:57:23 +0300

    Database File
    Import this or copy paste to your SQL command to create the table. You also add custom fields as you wish or as your project demands.

--- Tutorial 2/mobile_payments.sql
+++ Tutorial 2/mobile_payments.sql
@@ -1,22 +1,23 @@
 
-# Copy this code to your SQL query box and run. Ensure you have selected the database on config.php
-# Database not optimised for now.
+-- Copy this code to your SQL query box and run. Ensure you have selected the database on config.php
+-- Database not optimised for now.
+-- Custom fields can also be added
 
 CREATE table mobile_payments(
-	  transLoID int AUTO_INCREMENT not null,
-    TransactionType varchar(10) not null,
-    TransID varchar(10) not null,
-    TransTime varchar(14) not null,
-    TransAmount varchar(6) not null,
-    BusinessShortCode varchar(6) not null,
-    BillRefNumber varchar(6) not null,
-    InvoiceNumber varchar(6) not null,
-    OrgAccountBalance varchar(10) not null,
-    ThirdPartyTransID varchar(10) not null,
-    MSISDN varchar(14) not null,
-    FirstName varchar(10),
-    MiddleName varchar(10),
-    LastName varchar(10),
-    PRIMARY KEY (transLoID),
-    UNIQUE(TransID)
-);
+	transLoID int AUTO_INCREMENT not null,
+	TransactionType varchar(10) not null,
+	TransID varchar(10) not null,
+	TransTime varchar(14) not null,
+	TransAmount varchar(6) not null,
+	BusinessShortCode varchar(6) not null,
+	BillRefNumber varchar(6) not null,
+	InvoiceNumber varchar(6) not null,
+	OrgAccountBalance varchar(10) not null,
+	ThirdPartyTransID varchar(10) not null,
+	MSISDN varchar(14) not null,
+	FirstName varchar(10),
+	MiddleName varchar(10),
+	LastName varchar(10),
+	PRIMARY KEY (transLoID),
+	UNIQUE(TransID)
+) Engine=innoDB;
