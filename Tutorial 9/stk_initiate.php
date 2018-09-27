e38edb38d71a3e35a16d6242afcfdf1b8f1ccf5d
Ben Njunge <34906115+bnjunge@users.noreply.github.com>
2018-09-27 09:27:07 +0300

    Update stk_initiate.php

--- Tutorial 9/stk_initiate.php
+++ Tutorial 9/stk_initiate.php
@@ -1,32 +1,13 @@
 <?php
 
-  // access token
+  # access token
   $consumerKey = ''; //Fill with your app Consumer Key
   $consumerSecret = ''; // Fill with your app Secret
 
-  $headers = ['Content-Type:application/json; charset=utf8'];
-
-  $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-  $curl = curl_init($access_token_url);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($curl, CURLOPT_HEADER, FALSE);
-  curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
-  $result = curl_exec($curl);
-  $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-  $result = json_decode($result);
-  $access_token = $result->access_token;  
-  curl_close($curl);
-
-  # initiating the transaction
-
   # define the variales
-  $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-
   # provide the following details, this part is found on your test credentials on the developer account
   $BusinessShortCode = '';
-  $Passkey = '';
-  
+  $Passkey = '';  
   
   /*
     This are your info, for
@@ -43,15 +24,37 @@
   $TransactionDesc = '';
   $Amount = '';
  
-  
-  $Timestamp = date('YmdGis');  
-  $CallBackURL = 'http://kidonda.us/projects/MPESA_API/callback_url.php';  
+  # Get the timestamp, format YYYYmmddhms -> 20181004151020
+  $Timestamp = date('YmdGis');    
   
   # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
   $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
 
+  # header for access token
+  $headers = ['Content-Type:application/json; charset=utf8'];
+
+  # header for stk push
   $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
 
+  # M-PESA endpoint urls
+  $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+  # callback url
+  $CallBackURL = 'http://kidonda.us/projects/MPESA_API/callback_url.php';  
+
+  $curl = curl_init($access_token_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_HEADER, FALSE);
+  curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+  $result = curl_exec($curl);
+  $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+  $result = json_decode($result);
+  $access_token = $result->access_token;  
+  curl_close($curl);
+
+  # initiating the transaction
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, $initiate_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
@@ -72,11 +75,9 @@
   );
 
   $data_string = json_encode($curl_post_data);
-
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($curl, CURLOPT_POST, true);
   curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-
   $curl_response = curl_exec($curl);
   print_r($curl_response);
 
