b7bdfae299b1f09517e0c9edd025773355ee5f55
Ben Njunge <34906115+bnjunge@users.noreply.github.com>
2018-10-03 16:20:25 +0300

    ResultURL Code

---
+++ Tutorial 10/B2CResultURL.php
@@ -0,0 +1,8 @@
+<?php
+
+	$callbackResponse = file_get_contents('php://input');
+
+	$logFile = "B2CResultResponse.json";
+    $log = fopen($logFile, "a");
+    fwrite($log, $callbackResponse);
+    fclose($log);
